b21f5717d64137e3407dbab97c3275d973c3ac14
akram786khan <104996035+akram786khan@users.noreply.github.com>
2023-02-16 21:34:21 +0530

    index update

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('hello akram khan!')
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+const { errorHandler } = require('./backend/middleware/errorMiddleware')
+const ConnectDB = require('./backend/config/db');
+const { execPath } = require('process');
+const app = express();
+
+ConnectDB();
+// app.get("/", (req, res) => {
+//     res.status(200).json({ message: "get data" });
+// })
+app.use(errorHandler);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+app.use('/api/users', require('./backend/routes/userRoutes'))
+app.use('/api/products', require('./backend/routes/productRoutes'))
+app.listen(port, () => {
+    console.log(`app server started on port ${port}`)
 })
-app.listen(process.env.PORT || 3000)
