a9479b9400c9e967956d399ba22b412f49da1218
akram786khan <104996035+akram786khan@users.noreply.github.com>
2023-02-16 20:52:06 +0530

    dssss sds
    sds

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('hello akram!')
+    res.send('hello akram khan!')
 })
 app.listen(process.env.PORT || 3000)
