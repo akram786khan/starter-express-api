e881515c08db46643ccf83c4d455b9bf11ed239f
a <a@as-MacBook-Air.local>
2023-03-02 14:14:17 +0530

    students

--- backend/routes/dashbordRoutes.js
+++ backend/routes/dashbordRoutes.js
@@ -4,11 +4,22 @@ const users = require('../modals/studentModal/StudentModel');
 router.get('/Dashbord', async (req, res) => {
     const data = await users.find();
     arr = [
-        employes = {
+        Students = {
+            "image": "https://watermark.lovepik.com/photo/20211130/large/lovepik-primary-school-students-study-picture_501212451.jpg",
+            "name": "Students",
+            "data": data
+        },
+        Employes = {
             "image": "https://wallpaperaccess.com/full/4322025.jpg",
             "name": "Employes",
             "data": data
+        },
+        Products = {
+            "image": "https://stickybranding.com/wp-content/uploads/2019/11/How-to-name-your-product.jpg",
+            "name": "Products",
+            "data": data
         }
+
     ]
     res.status(200).json(arr)
 })
