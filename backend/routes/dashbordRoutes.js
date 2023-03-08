cfa382b7ca15a93a0202d2c05a67ccb2343e4400
a <a@as-MacBook-Air.local>
2023-03-08 15:21:23 +0530

    sign update

--- backend/routes/dashbordRoutes.js
+++ backend/routes/dashbordRoutes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const users = require('../modals/studentModal/StudentModel');
+const employe = require("../modals/employeModal/employeModal");
+const product = require('../modals/productModal');
 router.get('/Dashbord', async (req, res) => {
     const data = await users.find();
+    const employedata = await employe.find();
+    const productdata = await product.find();
+
     arr = [
         Students = {
             "image": "https://watermark.lovepik.com/photo/20211130/large/lovepik-primary-school-students-study-picture_501212451.jpg",
@@ -12,12 +17,12 @@ router.get('/Dashbord', async (req, res) => {
         Employes = {
             "image": "https://wallpaperaccess.com/full/4322025.jpg",
             "name": "Employes",
-            "data": data
+            "data": employedata
         },
         Products = {
             "image": "https://stickybranding.com/wp-content/uploads/2019/11/How-to-name-your-product.jpg",
             "name": "Products",
-            "data": data
+            "data": productdata
         }
 
     ]
