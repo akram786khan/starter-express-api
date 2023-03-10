e865013c6899a65bd7e593d2f7a34f18f8380817
a <a@as-MacBook-Air.local>
2023-03-10 14:30:31 +0530

    delete product

--- backend/routes/addtocartRoutes.js
+++ backend/routes/addtocartRoutes.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const { getCartProduct, addCartProduct } = require('../controllers/cartProducts/cartProducts')
 const { cartprotect } = require('../middleware/cartAuthMiddleware');
 router.get('/getCartProduct', cartprotect, getCartProduct)
-router.post('/addCartProduct', addCartProduct)
+router.post('/addCartProduct', cartprotect, addCartProduct)
 module.exports = router;
